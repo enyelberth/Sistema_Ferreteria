61ee73196cc1a44c7f91d81629c56bc28ddad588
enyelberth <enyelberthrc22.z@gmail.com>
2024-08-28 00:26:27 -0400

    feat: create get and set product and categor

--- Sistema_Ferreteria/Models/Category.cs
+++ Sistema_Ferreteria/Models/Category.cs
@@ -6,11 +6,28 @@ using System.Threading.Tasks;
 
 namespace Sistema_Ferreteria.Models
 {
-    internal class Categoria
+    internal class Category
     {
         private int id;
         private string name;
         private string creationDate;
         private string updateDate;
+
+        public int Id { 
+            get { return id; } 
+            set {    id = value; } 
+        }
+        public string Name {
+            get { return name; }
+            set { name = value; } 
+        }
+        public string CreationDate { 
+            get { return creationDate; } 
+            set {    creationDate = value; } 
+        }
+        public string UpdateDate { 
+            get { return updateDate; }
+            set { updateDate = value; }
+        }
     }
 }
--- Sistema_Ferreteria/Models/ProductModel.cs
+++ Sistema_Ferreteria/Models/ProductModel.cs
@@ -16,6 +16,35 @@ namespace Sistema_Ferreteria.models
         private int category;
         private int creationDate;
         private int updateDate;
-    
+
+
+
+        public int Id {
+            get { return id; } 
+            set{ id = value; }
+        }
+        public string Name {  
+            get { return name; } 
+            set{ name = value; }
+        }
+        public float BuyPrice {
+            get { return buyPrice; } 
+            set { buyPrice = value; } }
+        public float SalePrice {
+            get {  return salePrice; } 
+            set { salePrice = value; } 
+        }
+        public int Category { 
+            get { return category; }  
+            set { category = value; }
+        } 
+        public int CreationDate { 
+            get { return creationDate; }
+            set { creationDate = value; } 
+        }
+        public int UpdateDate { 
+            get {   return updateDate; } 
+            set { updateDate = value; } 
+        }
      }
 }
